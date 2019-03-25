b1576174ffc93dd03f368887dc0d9a6467315d93
Gyula Madarasz <gyula.madarasz@salesagility.com>
2019-03-25 13:52:55 +0000

    change the colour to more visible

--- projects/fancy-button/src/fancy-text/fancy-text.component.ts
+++ projects/fancy-button/src/fancy-text/fancy-text.component.ts
@@ -3,8 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-fancy-text',
     template: `
-    <p style="color: yellow;">
-        Its a fancy text and the customization wont affect it,
+    <p style="color: brown;">
+        Its a fancy text and the customization using it but wont affect it,
         however it could.
     </p>`,    
 })
